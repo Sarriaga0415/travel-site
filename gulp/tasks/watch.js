e06eef914257636711cba65178595c951b062a9e
Sophia <Sophia.L.Arriaga@gmail.com>
2019-05-28 00:36:25 -0400

    Brad's version of travel site

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,9 +1,8 @@
-var gulp = require("gulp"),
-  watch = require("gulp-watch"),
-  browserSync = require("browser-sync").create();;
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-
-gulp.task("watch", function() {
+gulp.task('watch', function() {
 
   browserSync.init({
     notify: false,
@@ -12,17 +11,17 @@ gulp.task("watch", function() {
     }
   });
 
-  watch("./app/index.html", function() {
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
 
-  watch("./app/assets/styles/**/*.css", function() {
-    gulp.start("cssInject");
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
   });
 
 });
 
-gulp.task("cssInject", ["styles"], function(){
-  return gulp.src("./app/temp/styles/styles.css")
-  .pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
