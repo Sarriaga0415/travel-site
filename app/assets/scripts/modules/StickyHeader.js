491748e2f1c9e5621a7073c2bc0c3fba8daee081
Sophia <Sophia.L.Arriaga@gmail.com>
2019-06-05 23:43:35 -0400

    Completed sticky header functionality

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,6 +1,7 @@
 //jshint esversion: 6
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
@@ -10,6 +11,11 @@ class StickyHeader {
     this.pageSections = $(".page-section");
     this.headerLinks = $(".primary-nav a");
     this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
   }
 
   createHeaderWaypoint() {
@@ -31,12 +37,26 @@ class StickyHeader {
       var currentPageSection = this;
       new Waypoint({
         element: currentPageSection,
-        handler: function() {
-          var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
-          that.headerLinks.removeClass("is-current-link");
-          $(matchingHeaderLink).addClass("is-current-link");
+        handler: function(direction) {
+          if (direction === "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
         },
         offset: "18%"
+      });
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction === "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "-40%"
       });
     });
   }
